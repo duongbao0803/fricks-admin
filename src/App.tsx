30edda1a7b7468e4ba960de523c237eb6e131fb1
Duong Bao <tonbao0803@gmail.com>
2024-09-28 14:11:39 +0700

    fix: Fix header

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import Router from "./routes/Sections";
 import { NotificationProvider } from "./components/Notification";
 import { useAuthStore } from "./hooks/useAuthStore";
 import { useEffect } from "react";
+import Cookies from "js-cookie";
 
 function App() {
   const { fetchUserInfo } = useAuthStore();
+  const accessToken = Cookies.get("accessToken");
 
   useEffect(() => {
     fetchUserInfo();
-  }, []);
+  }, [accessToken]);
 
   return (
     <>
--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -16,6 +16,7 @@ import { useAuthStore } from "@/hooks/useAuthStore";
 import ADMIN from "@/assets/images/avatar/avatar_admin.jpg";
 import STORE from "@/assets/images/avatar/avatar_staff.jpg";
 import { Roles } from "@/enums";
+import { Loading } from "@/components";
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -58,6 +59,7 @@ const items: MenuItem[] = [
 const DashboardLayout: React.FC<LayoutProps> = ({ children }) => {
   const [collapsed, setCollapsed] = useState<boolean>(true);
   const userInfo = useAuthStore((s) => s.userInfo);
+  const isLoading = useAuthStore((s) => s.isLoading);
 
   const logout = useAuthStore((s) => s.logout);
 
@@ -141,26 +143,29 @@ const DashboardLayout: React.FC<LayoutProps> = ({ children }) => {
         style={{ marginLeft: collapsed ? 55 : 230 }}
       >
         <div className="header fixed z-[999] flex h-16 items-center justify-end gap-2 bg-[#f8f8f8] bg-opacity-80 pr-4 shadow-none backdrop-blur-[6px]">
-          <>
-            <img
-              className="h-[42px] w-[42px] rounded-full border object-cover ring-2 ring-gray-300 hover:ring-[#0077ff]"
-              src={
-                userInfo && userInfo?.role?.includes(Roles.ADMIN)
-                  ? ADMIN
-                  : STORE
-              }
-            />
-          </>
-
-          <div className="flex flex-col">
-            <strong>{userInfo?.fullName || "Null"}</strong>
-            <div
-              className="cursor-pointer font-semibold text-[#5099ff] hover:underline"
-              onClick={handleLogout}
-            >
-              Đăng xuất
-            </div>
-          </div>
+          {isLoading ? (
+            <Loading></Loading>
+          ) : (
+            <>
+              <img
+                className="h-[42px] w-[42px] rounded-full border object-cover ring-2 ring-gray-300 hover:ring-[#0077ff]"
+                src={
+                  userInfo && userInfo?.role?.includes(Roles.ADMIN)
+                    ? ADMIN
+                    : STORE
+                }
+              />
+              <div className="flex flex-col">
+                <strong>{userInfo?.fullName || "Null"}</strong>
+                <div
+                  className="cursor-pointer font-semibold text-[#5099ff] hover:underline"
+                  onClick={handleLogout}
+                >
+                  Đăng xuất
+                </div>
+              </div>
+            </>
+          )}
         </div>
         <Content className="mx-4 mt-[80px]">
           <div className="min-w-[250px] overflow-x-auto rounded-xl ">
