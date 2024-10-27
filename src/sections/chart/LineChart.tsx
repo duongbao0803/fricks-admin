9b7aa7a0dac3baf6daddd09073601262f377544a
Dang Gia Duc <giaducdang@gmail.com>
2024-10-27 21:28:51 +0700

    fix: title main chart

--- src/sections/chart/LineChart.tsx
+++ src/sections/chart/LineChart.tsx
@@ -12,14 +12,17 @@ interface LineChartProps {
 const LineChart: React.FC<LineChartProps> = (props) => {
   const { chartData, options } = props;
 
-  const startOfWeek = dayjs().startOf('week'); // Start of week (Sunday)
-  const endOfWeek = startOfWeek.add(7, 'days'); // Move to the next Sunday
+  const currentDate = dayjs();
+  const startOfWeek = currentDate.day() === 0 
+    ? currentDate.day(-6).startOf("day")
+    : currentDate.day(1).startOf("day"); // start week (Monday)
+  const endOfWeek = startOfWeek.add(6, 'days').endOf('day'); // end week (Sunday)  
 
   return (
     <>
       <div className="w-full  p-5 ">
         <div className="mb-10">
-          <p className="text-xl font-bold">Đơn hàng và Doanh thu từ ngày {' '}
+          <p className="text-xl font-bold">Đơn hàng và doanh thu từ ngày {' '}
             {`${startOfWeek.format('DD/MM/YYYY')} đến ${endOfWeek.format('DD/MM/YYYY')}`}
           </p>
         </div>
